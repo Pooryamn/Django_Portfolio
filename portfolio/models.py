fe045359d52f9c3b83eb698093190ee65aec4b06
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-12 17:51:39 +0430

    create a model for project in portfolio app

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    # Property
+    # in my web site, a project have a Title,Description,Image,Url
+    Title = models.CharField(max_length=100) #maximum characters = 100
+    Description = models.CharField(max_length=300)
+    Image = models.ImageField(upload_to='portfolio/images/') # image will upload to this directory
+    Url = models.URLField(blank=True)# balnk=True means the field will not be required
+
+
+
+    
