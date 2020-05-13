958896ea1db6bb702578e9a917d9e34b726c0cf4
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-14 03:39:21 +0430

    forwarding urls... that means any request that is about blog will send to blog app and handle there

--- Personal_Portfolio/urls.py
+++ Personal_Portfolio/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home')
+    path('',views.home,name='home'),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
