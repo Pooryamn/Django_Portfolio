230aa064b77cbe2134e02b54da73028a9a8cdf62
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-14 03:23:06 +0430

    fetch objects from db in Django

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def home(request):
-    return render(request,'portfolio/home.html')
+    Projects = Project.objects.all()
+    return render(request,'portfolio/home.html',{'projects':Projects})
