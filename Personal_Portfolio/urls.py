10e660703791869ba4794be83d3022d1df447990
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-14 02:44:10 +0430

    home url added to site urls(urls.py file)

--- Personal_Portfolio/urls.py
+++ Personal_Portfolio/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='home')
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
