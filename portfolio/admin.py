d791d960c4f8f522240614222b2696ce7d8952ab
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-15 01:26:22 +0430

    blog added to admin part

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from .models import Project
-from blog.models import Blog
 
 # Register your models here.
 
 admin.site.register(Project) # add Project Model inside admin page
-admin.site.register(Blog)
+
