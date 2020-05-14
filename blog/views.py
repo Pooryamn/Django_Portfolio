ecefd415978f21c9ffd20485107b545b83cd92b9
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-15 01:40:45 +0430

    get blog object from database and pass in to webpage

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 def blog(request):
-    return render(request,'blog/blog.html')
+    Blogs = Blog.objects.all()
+    return render(request,'blog/blog.html',{'blogs':Blogs})
