10e09007c7e63fc113eb47312f0ede497b79dd12
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-15 01:22:02 +0430

    make a model for blog objects

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    Title = models.CharField(max_length=100)
+    Date = models.DateField()
+    Description = models.TextField()
