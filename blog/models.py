6e4221ccd48e912ee3dccff9bab3e6cca6faed03
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-16 19:26:12 +0430

    show title of blog in admin part

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,7 @@ class Blog(models.Model):
     Title = models.CharField(max_length=100)
     Date = models.DateField()
     Description = models.TextField()
+
+
+    def __str__(self):
+        return self.Title
