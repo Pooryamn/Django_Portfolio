12661131015191d8ab1587dfd906cdb5c752846b
pooryamohammdadi <poorya.m.n.b@gmail.com>
2020-05-16 18:32:56 +0430

    get special object from DB using ID(pk).or if it does not exist show 404 error

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -7,4 +7,5 @@ def blog(request):
     return render(request,'blog/blog.html',{'blogs':Blogs})
 
 def detail(request,blog_id):
-    return render(request,'blog/detail.html',{'id':blog_id})
+    blog = get_object_or_404(Blog,pk=blog_id)
+    return render(request,'blog/detail.html',{'blog':blog})
